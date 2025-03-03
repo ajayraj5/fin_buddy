8492ea5c555a116d74a8be5eb2d13169f06e6409
ajayraj5 <ajaymahiwal7@gmail.com>
2025-03-03 13:38:39 +0530

    fix: open headless

--- fin_buddy/events/incometax_gov.py
+++ fin_buddy/events/incometax_gov.py
@@ -44,6 +44,9 @@ def setup_chrome_options(user_download_dir):
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_experimental_option("useAutomationExtension", False)
     options.add_experimental_option('excludeSwitches', ['enable-automation'])
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
 
     prefs = {
         "download.default_directory": user_download_dir,
